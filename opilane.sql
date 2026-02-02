359d9b8c5c4408612a6bc50234210d429a82a80f
Oleg Bereževski <116960300+z1znenno@users.noreply.github.com>
2026-02-02 16:05:39 +0200

    Modify opilane table structure and add data examples
    Updated perekonnanimi to be unique and added data insertion examples.

--- opilane.sql
+++ opilane.sql
@@ -6,9 +6,26 @@ create table opilane
 (
 	opilaneID int primary key identity(1, 1),
 	eesnimi varchar(25),
-	perekonnanimi varchar(30) not null,
+	perekonnanimi varchar(30) not null unique,
 	synniaeg date,
 	aadress text,
 	kas_opib bit
 );
+-- kuvab tabeli, * - kõik väljad
 select * from opilane;
+
+--Tabeli kustutamine
+drop table opilane;
+
+--Andmete lisamine tabelisse opilane
+insert into opilane(eesnimi, perekonnanimi, synniaeg, kas_opib)
+values ('Yarik', 'Berezevski', '2025-12-12', 1)
+select * from opilane
+
+--Lisamine 3. kirjet korraga
+insert into opilane(eesnimi, perekonnanimi, synniaeg, kas_opib)
+values ('Yarik', 'Berezevski', '2025-12-12', 1),
+('Yarik', 'Berezevs', '2025-12-12', 1),
+('Yarik', 'Berezevsi', '2025-12-12', 1),
+('Yarik', 'Berezev', '2025-12-12', 1);
+select * from opilane
